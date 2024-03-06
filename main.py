61aeaec68a5972bfe4f6c468cade715be489d383
Sebabas <sebasmtzsanchez@gmail.com>
2024-03-06 14:05:52 -0600

    Se añaden las funciones para recortar tamaño y tomar foto

--- main.py
+++ main.py
@@ -7,29 +7,54 @@ video = None
 
 def video_camara():
   global video
-  video = cv2.VideoCapture(0)
+  video = cv2.VideoCapture(1)
   iniciar()
 
 def iniciar():
-  global videoret 
+  global video
   ret, frame = video.read()
   if ret == True:
-    frame = imutils.resize(frame, width=640)
-    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    img = Image.fromarray(frame)
-    image = ImageTk.PhotoImage(image=img)
-    etiqueta_video.configure(image=image)
-    etiqueta_video.image = image
-    etiqueta_video.after(10,iniciar)
+        frame = imutils.resize(frame, width=700)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(frame)
+        img_recortada = recortar_tamano_infantil(img)
+        img_tk_recortada = ImageTk.PhotoImage(img_recortada)
+        etiqueta_video.configure(image=img_tk_recortada)
+        etiqueta_video.image = img_tk_recortada
+        etiqueta_video.after(10, iniciar)
 
+def recortar_tamano_infantil(imagen):
+    x1, y1, x2, y2 = 80, 50, 475, 530
+    imagen_recortada = imagen.crop((x1, y1, x2, y2))
+    return imagen_recortada
+
+def tomar_foto():
+    global video
+    ret, frame = video.read()
+    if ret == True:
+        frame = imutils.resize(frame, width=640)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        img = Image.fromarray(frame)
+        img_recortada = recortar_tamano_infantil(img)
+        ventana_foto = tk.Toplevel(root)
+        ventana_foto.title("Foto Capturada")
+
+        img_tk_recortada = ImageTk.PhotoImage(img_recortada)
+        etiqueta_foto_recortada = tk.Label(ventana_foto, image=img_tk_recortada)
+        etiqueta_foto_recortada.image = img_tk_recortada
+        etiqueta_foto_recortada.pack()
+        
 root = tk.Tk()
 root.state('zoomed')
 
 fondo_inicio = tk.PhotoImage(file="./img/INICIO.png")
 fondo_inicio_label = tk.Label(root, image=fondo_inicio).place(x=0,y=0,relwidth=1,relheight=1)
 
-iniciar_cam_btn = tk.Button(root, text="Iniciar Cámara", bg="#32bea6",relief="flat",cursor="hand2",command=video_camara,width=15,height=3,font=("Calisto MT",12,"bold")).place(x=640,y=720)
+iniciar_cam_btn = tk.Button(root, text="Iniciar Cámara", bg="#32bea6",cursor="hand2",command=video_camara,width=15,height=3,font=("Calisto MT",12,"bold")).place(x=240,y=720)
+
+tomar_foto_btn = tk.Button(root, text="Tomar fotografía", bg="#32bea6",cursor="hand2",command=tomar_foto,width=15,height=3,font=("Calisto MT",12,"bold")).place(x=540,y=720)
 
 etiqueta_video = tk.Label(root,bg="white")
 etiqueta_video.place(x=520,y=225)
+
 root.mainloop()
